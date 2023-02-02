d8b49a66660b31bfff0c23d5b269d8074099f293
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-02-01 20:59:36 -0500

    Check for authenticated user and matching company id.

--- server/resolvers.js
+++ server/resolvers.js
@@ -24,7 +24,12 @@ export const resolvers = {
             rejectIf(job.companyId !== user.companyId)
             return Job.delete(id)
         },
-        updateJob: (_root, { input }) => Job.update(input),
+        updateJob: async (_root, { input }, { user }) => { 
+            rejectIf(!user)
+            const job = await Job.findById(input.id)
+            rejectIf(job.companyId !== user.companyId)
+            return Job.update({ ...input, companyId: user.companyId })
+        }
     },
 
     Company: {
